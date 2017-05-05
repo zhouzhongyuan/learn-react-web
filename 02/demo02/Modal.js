a692c0b9acd2ad5804620245e4f936607dd28afb
zhouzhongyuan <yawenzhongyuan@gmail.com>
2017-05-05 17:10:17 +0800

    Fix wrong path

--- 02/demo02/Modal.js
+++ 02/demo02/Modal.js
@@ -1,35 +1,37 @@
 import React, { Component } from 'react';
-import { Modal, Text, TouchableHighlight, View } from 'react-native';
+import { Modal, Text, TouchableHighlight, View } from 'react-native/src/';
 
 class ModalExample extends Component {
     constructor(props){
         super(props);
         this.state = {
-            modalVisible: false,
+            modalVisible: true,
         }
         this.setModalVisible = this.setModalVisible.bind(this);
     }
 
-
-
     setModalVisible(visible) {
+        console.log(visible);
         this.setState({modalVisible: visible});
     }
 
     render() {
         return (
-            <View style={{marginTop: 22}}>
+            <View style={{display:'flex',flex:1}}>
                 <Modal
                     animationType={"slide"}
                     transparent={false}
                     visible={this.state.modalVisible}
                 >
-                    <View style={{marginTop: 22}}>
-                        <Text>Modal!</Text>
+                    <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+                        <View>
+                            <Text>Modal!</Text>
+                        </View>
                     </View>
                 </Modal>
 
                 <TouchableHighlight onPress={() => {
+                    console.log('Press');
                     this.setModalVisible(true)
                 }}>
                     <Text>Show Modal</Text>
